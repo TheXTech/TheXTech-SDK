3866fa4b680f7589eb1005ce573e050a200e47c8
Vitaly Novichkov <admin@wohlnet.ru>
2019-07-24 04:16:07 +0300

    Old SMBX time fix

--- script/utils/smbx_utils.lua
+++ script/utils/smbx_utils.lua
@@ -1,15 +1,17 @@
 local smbx_utils = {}
 
-local SMBXFrameSpeed = 65.0
+-- local SMBXFrameSpeed = 65.0
 --1000.0 / SMBXFrameSpeed
-local SMBXTickTime = 15.285
+-- local SMBXTickTime = 15.285
+local SMBXTickTime = 15.6
 
 function smbx_utils.ticksToTime(ticks)
-    return ticks * SMBXTickTime
+    -- return ticks * SMBXTickTime
+    return (ticks * 39) / 2.5;
 end
 
-function smbx_utils.speedConv(speed, timeTicks)
-        return speed * (timeTicks/SMBXTickTime)
+function smbx_utils.speedConv(speed, timeDelay)
+    return speed * ((timeDelay * 2.5) / 39.0);
 end
 
 return smbx_utils

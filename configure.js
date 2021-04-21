ad494a478b19167920d64197779e58c263995db8
Wohlstand <admin@wohlnet.ru>
2021-04-22 01:45:46 +0300

    Update the configure script

--- configure.js
+++ configure.js
@@ -18,17 +18,17 @@ function onConfigure()
         return false;
     }
 
-    var ini = INI.open( FileIO.scriptPath()+"/main.ini");
+    var ini = INI.open(FileIO.scriptPath() + "/main.ini");
 
     while(1)
     {
-        smbxPath = FileIO.getOpenDirPath("Please select your directory with installed SMBX or TheXTech to set up the SMBX Integration configuration package...", smbxPath );
+        smbxPath = FileIO.getOpenDirPath("Please select your directory with installed SMBX/TheXTech to set up the XTech SDK configuration package...", smbxPath );
         if(smbxPath=="")
         {
             PGE.msgBoxWarning("Configuring has been canceled",
-            "You was canceled a configuring of the\n'SMBX Intergation configuration package'!\n"+
-            "To take able use it, you should choice the path to your installed SMBX or TheXTech.\n\n"+
-            "SMBX Integration config pack was not configured!" );
+            "You was canceled a configuring of the\n'TheXTech SDK configuration package'!\n"+
+            "To take able use it, you should choice the path to your installed SMBX/TheXTech.\n\n"+
+            "TheXTech SDK config pack was not configured!" );
             return false;
         }
 
@@ -41,23 +41,16 @@ function onConfigure()
                 throw("'"+smbxPath+"/graphics/npc"+"' directory not exists");
 
             var smbxEXENames = [
-                "smbx.legacy",
-                "smbx.exe.legacy",
                 "smbx.exe",
-                "smbx1301.exe",
-                "smbx13.exe",
-                "asmbxt.exe",
-                "a2mbxt.exe",
-                "a2xt.exe",
                 "smbx-win64.exe",
                 "thextech.exe",
                 "advdemo.exe",
                 "smbx",
                 "thextech",
-                "advdemo",
-                ];
+                "advdemo"
+            ];
 
-            for(var i=0; i<smbxEXENames.length; i++)
+            for(var i=0; i < smbxEXENames.length; i++)
             {
                 if(FileIO.isFileExists(smbxPath+"/"+smbxEXENames[i]))
                 {
@@ -77,189 +70,20 @@ function onConfigure()
 
             ini.setValue("application-path-configured", true);
 
-            if(FileIO.isDirExists(smbxPath+"/music"))
-                ini.setValue("music", "music");
-            else if(FileIO.isDirExists(smbxPath+"/music_ogg"))
-                ini.setValue("music", "music_ogg");
-
-            if(FileIO.isDirExists(smbxPath+"/sound"))
-                ini.setValue("sound", "sound");
-            else if(FileIO.isDirExists(smbxPath+"/sound_ogg"))
-                ini.setValue("sound", "sound_ogg");
-
             ini.close();
-
-            //  Generate dummy elements gifs needed for PGE
-
-            /*
-            Total worldmap tiles usable: "tile-1" to "tile-400" (tile 401 is not usable anymore). There are 72 new tiles.
-            Total paths usable: "path-1" to "path-100" (path 101 is not usable anymore). There are 68 new paths, all using a mask/transparency.
-            Total levels usable: "level-1" to "level-100" (level 101 is not usable anymore). There are 68 new levels, all using a mask/transparency.
-            Total sceneries usable: "scene-1" to "scene-100" (scene 101 is not usable anymore). There are 35 new sceneries, all using a mask/transparency.
-            Total blocks usable: "block-1" to "block-700" (block 701 is not usable anymore). There are 62 new blocks which are all using a mask/transparency.
-            Total BGOs usable: "background-1" to "background-200" (background 201 is not usable anymore). There are 10 new BGOs which are all using a mask/transparency.
-            There are no additional level-backgrounds ("background2-XX") usable, though.
-            */
-
-            for(var i=292; i<=300; i++)
-            {
-                var inputfile = FileIO.scriptPath()+"/commonGFX/dummy-npc.gif";
-                var outputfile = smbxPath+"/graphics/npc/npc-" + i + ".gif";
-                FileIO.copy( inputfile, outputfile, false );
-
-                    inputfile = FileIO.scriptPath()+"/commonGFX/dummy-npcm.gif";
-                    outputfile = smbxPath+"/graphics/npc/npc-" + i + "m.gif";
-                FileIO.copy( inputfile, outputfile, false );
-            }
-            for(var i=639; i<=700; i++)
-            {
-                var inputfile = FileIO.scriptPath()+"/commonGFX/dummy-block.gif";
-                var outputfile = smbxPath+"/graphics/block/block-" + i + ".gif";
-                FileIO.copy( inputfile, outputfile, false );
-            }
-            for(var i=191; i<=200; i++)
-            {
-                var inputfile = FileIO.scriptPath()+"/commonGFX/dummy_bgo.gif";
-                var outputfile = smbxPath+"/graphics/background/background-" + i + ".gif";
-                FileIO.copy( inputfile, outputfile, false );
-            }
-            for(var i=329; i<=400; i++)
-            {
-                var inputfile = FileIO.scriptPath()+"/commonGFX/dummy_tile.gif";
-                var outputfile = smbxPath+"/graphics/tile/tile-" + i + ".gif";
-                FileIO.copy( inputfile, outputfile, false );
-            }
-            for(var i=66; i<=100; i++)
-            {
-                var inputfile = FileIO.scriptPath()+"/commonGFX/dummy_scene.gif";
-                var outputfile = smbxPath+"/graphics/scene/scene-" + i + ".gif";
-                FileIO.copy( inputfile, outputfile, false );
-            }
-            for(var i=33; i<=100; i++)
-            {
-                var inputfile = FileIO.scriptPath()+"/commonGFX/dummy_path.gif";
-                var outputfile = smbxPath+"/graphics/path/path-" + i + ".gif";
-                FileIO.copy( inputfile, outputfile, false );
-            }
-            for(var i=33; i<=100; i++)
-            {
-                var inputfile = FileIO.scriptPath()+"/commonGFX/dummy_wlvl.gif";
-                var outputfile = smbxPath+"/graphics/level/level-" + i + ".gif";
-                FileIO.copy( inputfile, outputfile, false );
-            }
-
-            /* Ingereting of sounds and musics from target SMBX assembly */
-            FileIO.copy( FileIO.scriptPath()+"/util/music-vanilla.ini", FileIO.scriptPath()+"/music.ini", true );
-
-            if(FileIO.isFileExists(smbxPath+"/music.ini"))
-            {
-                var vMusics  = INI.open( FileIO.scriptPath() + "/music.ini" );
-                var llMusics = INI.open( smbxPath + "/music.ini" );
-
-                for(var i=1; i<=16; i++)
-                {
-                    var section = "world-music-" + i;
-                    llMusics.beginGroup(section);
-                    vMusics.beginGroup(section);
-                    var sndname = llMusics.value("file", "");
-                    if( sndname != "" )
-                    {
-                        var last_slash_idx = sndname.lastIndexOf("/");
-                        if( last_slash_idx != -1 )
-                        {
-                            sndname = sndname.substr(last_slash_idx+1, sndname.length-1);
-                        }
-                        vMusics.setValue("file", sndname );
-                    }
-                    llMusics.endGroup();
-                    vMusics.endGroup();
-                }
-
-                for(var i=1; i<=3; i++)
-                {
-                    var section = "special-music-" + i;
-                    llMusics.beginGroup(section);
-                    vMusics.beginGroup(section);
-                    var sndname = llMusics.value("file", "");
-                    if( sndname != "" )
-                    {
-                        var last_slash_idx = sndname.lastIndexOf("/");
-                        if( last_slash_idx != -1 )
-                        {
-                            sndname = sndname.substr(last_slash_idx+1, sndname.length-1);
-                        }
-                        vMusics.setValue("file", sndname );
-                    }
-                    llMusics.endGroup();
-                    vMusics.endGroup();
-                }
-
-                for(var i=1; i<=56; i++)
-                {
-                    if(i==24) continue;
-                    var section = "level-music-" + i;
-                    llMusics.beginGroup(section);
-                    vMusics.beginGroup(section);
-                    var sndname = llMusics.value("file", "");
-                    if( sndname != "" )
-                    {
-                        var last_slash_idx = sndname.lastIndexOf("/");
-                        if( last_slash_idx != -1 )
-                        {
-                            sndname = sndname.substr(last_slash_idx+1, sndname.length-1);
-                        }
-                        vMusics.setValue("file", sndname );
-                    }
-                    llMusics.endGroup();
-                    vMusics.endGroup();
-                }
-
-                llMusics.close();
-                vMusics.close();
-            }
-
-            FileIO.copy( FileIO.scriptPath()+"/util/sounds-vanilla.ini", FileIO.scriptPath()+"/sounds.ini", true );
-
-            if(FileIO.isFileExists(smbxPath+"/sounds.ini"))
-            {
-                var vSounds  = INI.open( FileIO.scriptPath() + "/sounds.ini" );
-                var llSounds = INI.open( smbxPath + "/sounds.ini" );
-
-                for(var i=1; i<=91; i++)
-                {
-                    var section = "sound-" + i;
-                    llSounds.beginGroup(section);
-                    vSounds.beginGroup(section);
-                    var sndname = llSounds.value("file", "");
-                    if( sndname != "" )
-                    {
-                        var last_slash_idx = sndname.lastIndexOf("/");
-                        if( last_slash_idx != -1 )
-                        {
-                            sndname = sndname.substr( last_slash_idx+1, sndname.length-1);
-                        }
-                        vSounds.setValue("file", sndname );
-                    }
-                    llSounds.endGroup();
-                    vSounds.endGroup();
-                }
-                llSounds.close();
-                vSounds.close();
-            }
-
         }
         catch(e)
         {
-            PGE.msgBoxWarning( "SMBX Integration configuration error",
-                               "This is a not SMBX directory!\nPlease try again!\n" + e);
+            PGE.msgBoxWarning( "TheXTech SDK configuration error",
+                               "This is a not TheXTech directory!\nPlease try again!\n" + e);
             continue;
         }
         break;
     }
 
-    PGE.msgBoxInfo( "SMBX Integration configured",
+    PGE.msgBoxInfo( "TheXTech SDK configured",
                     "Integration configuration pack successfully configured!\n"+
-                    "SMBX path is:\n" + smbxPath + "/" + SMBXExeName);
+                    "TheXTech path is:\n" + smbxPath + "/" + SMBXExeName);
 
     return true;
 }
